7845a3149e5d0625b831e38dd47efc41ce98acda
KimBoseung7 <kimboseung7@gmail.com>
2024-01-18 13:47:10 +0900

    d

---
+++ src/main/java/com/example/demo/Controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+//@Controller
+//@RequestMapping("/board")
+//public class BoardController {
+//	@Autowired
+//	BoardService service;
+//	@GetMapping("/main")
+//	public void main( {
+//	
+//	}
+//
+//}
